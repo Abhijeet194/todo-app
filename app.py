de93dbfb7885c942c6e2b1214836d0cbdd32ac80
Abhijeet194 <abhijeetkumar20059955@gmail.com>
2026-04-15 12:23:50 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+def ist_time():
+    return datetime.now(pytz.timezone("Asia/Kolkata"))
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500), nullable=True)
+    date_created = db.Column(db.DateTime, default=ist_time)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, description=desc)
+        db.session.add(todo)
+        db.session.commit()
+
+
+        return redirect('/')
+    all_todos = Todo.query.all()
+    return render_template('index.html', all_todos=all_todos)
+
+@app.route('/show')
+def products():
+    all_todos = Todo.query.all()
+    print(all_todos)
+    return 'this is products page'
+
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    todo = Todo.query.get(sno)
+
+    if request.method == 'POST':
+        todo.title = request.form['title']
+        todo.description = request.form['desc']
+
+        db.session.commit()
+        return redirect('/')
+    return render_template('update.html', todo=todo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.get(sno)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
